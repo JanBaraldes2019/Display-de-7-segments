8645b3b09d21f7217216288b41d5dc9cf7c1077c
JanBaraldes2019 <janbaraldes@gmail.com>
2020-01-15 23:23:56 +0100

    Create programa1.ino

---
+++ programa1/programa1.ino
@@ -0,0 +1,83 @@
+/**********************************************************************************
+**                                                                               **
+**                              Array de 8 LEDs                                  **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+//********************************* Includes **************************************
+
+//********************************* Variables *************************************
+
+const int led1 = 1;
+const int led2 = 2;
+const int led3 = 3;
+const int led4 = 4;
+const int led5 = 5;
+const int led6 = 6;
+const int led7 = 7;
+
+const int buttonPin = 8;
+
+//*********************************** SETUP **************************************
+
+void setup()
+{
+  DDRD = B11111111;
+  pinMode(buttonPin,INPUT);
+}
+//************************************ LOOP *************************************
+
+void loop()
+{
+  
+ if (digitalRead(buttonPin)==0)
+ {
+   PORTD = B0000000;
+   
+ }
+  else{
+  
+   numero0();   
+   delay(1000);
+   numero0();
+   numero1();
+   delay(1000);
+   numero0();
+   numero2();
+   delay(1000);
+   numero0();
+   numero3();
+   delay(1000); 
+   numero0();
+   numero4();
+   delay(1000);
+   numero0();
+   numero5();
+   delay(1000);
+   numero0();
+   numero6();
+   delay(1000);
+   numero0();
+   numero7();
+   delay(1000);
+   numero0();
+   numero8();
+   delay(1000);
+   numero0();
+   numero9();
+   delay(1000);
+    
+  }
+}
+//*************************************** FUNCIONS ********************************
+
+void numero0(){PORTD = B01111111;}
+void numero1(){PORTD = B00001100;}
+void numero2(){PORTD = B10110110;}
+void numero3(){PORTD = B10011111;}
+void numero4(){PORTD = B11001100;}
+void numero5(){PORTD = B11011011;}
+void numero6(){PORTD = B11111011;}
+void numero7(){PORTD = B00001110;}
+void numero8(){PORTD = B11111111;}
+void numero9(){PORTD = B11011111;}
